90736d5fbecb8540497af940b2e6ea6f24f8e78a
Peter Ahinko <peter@ahinko.se>
2014-12-31 00:00:22 +0100

    Added more Exceptions/Response codes

---
+++ src/Phapi/Exception/Accepted.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Phapi\Exception;
+
+use Phapi\Exception;
+
+/**
+ * Class Accepted
+ *
+ * Request accepted and set to be performed in a background task. Useful if your client is
+ * requesting something on the API that is time-consuming and you don?t want the client to have to wait.
+ *
+ * @category Exception
+ * @package  Phapi\Exception
+ * @author   Peter Ahinko <peter@ahinko.se>
+ * @license  MIT (http://opensource.org/licenses/MIT)
+ * @link     https://github.com/ahinko/phapi
+ */
+class Accepted extends Exception {
+
+    /**
+     * Response status code
+     *
+     * @var int
+     */
+    protected $statusCode = 203;
+
+    /**
+     * Response status message
+     *
+     * @var string
+     */
+    protected $statusMessage = 'Accepted';
+
+}
---
+++ src/Phapi/Exception/NoContent.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Phapi\Exception;
+
+use Phapi\Exception;
+
+/**
+ * Class No Content
+ *
+ * The response does not include any content.
+ *
+ * @category Exception
+ * @package  Phapi\Exception
+ * @author   Peter Ahinko <peter@ahinko.se>
+ * @license  MIT (http://opensource.org/licenses/MIT)
+ * @link     https://github.com/ahinko/phapi
+ */
+class NoContent extends Exception {
+
+    /**
+     * Response status code
+     *
+     * @var int
+     */
+    protected $statusCode = 204;
+
+    /**
+     * Response status message
+     *
+     * @var string
+     */
+    protected $statusMessage = 'No Content';
+
+}
