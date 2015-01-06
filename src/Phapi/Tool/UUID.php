59cb1939618fc7c257eefd235cf4a4d8e230a644
Peter Ahinko <peter@ahinko.se>
2015-01-06 22:02:10 +0100

    Added UUID tool.

---
+++ src/Phapi/Tool/UUID.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace Phapi\Tool;
+
+/**
+ * UUID generator class
+ *
+ * Class for generating UUIDs
+ *
+ * @category Tool
+ * @package  Phapi
+ * @author   Peter Ahinko <peter@ahinko.se>
+ * @license  MIT (http://opensource.org/licenses/MIT)
+ * @link     https://github.com/ahinko/phapi
+ */
+class UUID
+{
+
+    /**
+     * Generate a UUID
+     *
+     * If no parameters are included a v4 UUID will be generated
+     * else v5.
+     */
+    public static function generate($namespace = null, $name = null)
+    {
+        if ($namespace !== null && $name !== null) {
+            return self::v5($namespace, $name);
+        }
+
+        return self::v4();
+    }
+
+    /**
+     *
+     * Generate v4 UUID
+     *
+     * Version 4 UUIDs are pseudo-random.
+     *
+     * @return  string
+     */
+    public static function v4()
+    {
+        if (function_exists('openssl_random_pseudo_bytes')) {
+            $data = openssl_random_pseudo_bytes(16);
+        } else {
+            $data = file_get_contents('/dev/urandom', null, null, 0, 16);
+        }
+
+        $data[6] = chr(ord($data[6]) & 0x0f | 0x40); // set version to 0100
+        $data[8] = chr(ord($data[8]) & 0x3f | 0x80); // set bits 6-7 to 10
+
+        return vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
+    }
+
+    /**
+     * Generate v5 UUID
+     *
+     * Version 5 UUIDs are named based. They require a namespace (another
+     * valid UUID) and a value (the name). Given the same namespace and
+     * name, the output is always the same.
+     *
+     * @param	uuid	$namespace
+     * @param	string	$name
+     * @return  string
+     */
+    public static function v5($namespace, $name)
+    {
+        if (!self::isValid($namespace)) {
+            return false;
+        }
+
+        // Get hexadecimal components of namespace
+        $nhex = str_replace(['-','{','}'], '', $namespace);
+
+        // Binary Value
+        $nstr = '';
+
+        // Convert Namespace UUID to bits
+        for ($i = 0; $i < strlen($nhex); $i+=2) {
+            $nstr .= chr(hexdec($nhex[$i].$nhex[$i+1]));
+        }
+
+        // Calculate hash value
+        $hash = sha1($nstr . $name);
+
+        return sprintf(
+            '%08s-%04s-%04x-%04x-%12s',
+            // 32 bits for "time_low"
+            substr($hash, 0, 8),
+            // 16 bits for "time_mid"
+            substr($hash, 8, 4),
+            // 16 bits for "time_hi_and_version",
+            // four most significant bits holds version number 5
+            (hexdec(substr($hash, 12, 4)) & 0x0fff) | 0x5000,
+            // 16 bits, 8 bits for "clk_seq_hi_res",
+            // 8 bits for "clk_seq_low",
+            // two most significant bits holds zero and one for variant DCE1.1
+            (hexdec(substr($hash, 16, 4)) & 0x3fff) | 0x8000,
+            // 48 bits for "node"
+            substr($hash, 20, 12)
+        );
+    }
+
+    public static function isValid($uuid)
+    {
+        return preg_match(
+            '/^\{?[0-9a-f]{8}\-?[0-9a-f]{4}\-?[0-9a-f]{4}\-?'.
+            '[0-9a-f]{4}\-?[0-9a-f]{12}\}?$/i',
+            $uuid
+        ) === 1;
+    }
+}
