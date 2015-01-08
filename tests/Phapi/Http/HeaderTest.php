3e20127b9be487f0c4add4eccf9a8b0578035ad6
Peter Ahinko <peter@ahinko.se>
2015-01-08 22:49:29 +0100

    Added more tests for Header class.

--- tests/Phapi/Http/HeaderTest.php
+++ tests/Phapi/Http/HeaderTest.php
@@ -118,6 +118,7 @@ class HeaderTest extends \PHPUnit_Framework_TestCase
     /**
      * @covers ::set
      * @covers ::get
+     * @covers ::parseCacheControl
      */
     public function testSet()
     {
@@ -156,6 +157,7 @@ class HeaderTest extends \PHPUnit_Framework_TestCase
     /**
      * @covers ::addCacheControlDirective
      * @covers ::getCacheControlDirective
+     * @covers ::getCacheControlHeader
      */
     public function testAddCacheControlDirective()
     {
@@ -171,5 +173,75 @@ class HeaderTest extends \PHPUnit_Framework_TestCase
     {
         $this->headers->removeCacheControlDirective('max-age');
         $this->assertFalse($this->headers->hasCacheControlDirective('max-age'));
+    }
+
+    /**
+     * @covers ::count
+     */
+    public function testCount()
+    {
+        $this->assertEquals(10, $this->headers->count());
+    }
+
+    /**
+     * @covers ::getIterator
+     */
+    public function testGetIterator()
+    {
+        $this->assertInstanceOf('\ArrayIterator', $this->headers->getIterator());
+    }
+
+    /**
+     * @covers ::getDate
+     * @covers ::add
+     * @covers ::set
+     */
+    public function testGetDate()
+    {
+        $this->headers->add(['date' => 'Mon, 15 Aug 2005 15:52:01 +0000']);
+        $this->assertInstanceOf('\DateTime', $this->headers->getDate('date'));
+        $this->assertEquals(1124121121, $this->headers->getDate('date')->getTimestamp());
+
+        $this->headers->set('date', null);
+        $this->assertEquals(null, $this->headers->getDate('date'));
+    }
+
+    /**
+     * @covers ::getDate
+     */
+    public function testGetDateException()
+    {
+        $this->headers->add(['date' => 'Mon, Aug 15 2005 15:52:01 +0000']);
+        $this->setExpectedException('\RuntimeException');
+        $this->headers->getDate('date');
+    }
+
+    /**
+     * @covers ::__toString
+     */
+    public function testToStringEmpty()
+    {
+        $headers = new Header([]);
+        $this->assertEquals('', $headers->__toString());
+    }
+
+    /**
+     * @covers ::__toString
+     */
+    public function testToString()
+    {
+        $this->assertEquals(
+            'Accept:          text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8
+Accept-Encoding: gzip, deflate, sdch
+Accept-Language: en-US,en;q=0.8,sv;q=0.6
+Cache-Control:   max-age=0
+Connection:      keep-alive
+Content-Length:  302
+Content-Type:    application/json
+Dnt:             1
+Host:            localhost.dev
+User-Agent:      Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36
+'
+            , $this->headers->__toString());
     }
 }
