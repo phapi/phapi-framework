039823286eb782e5ad56083212e798f0df1d5685
Peter Ahinko <peter@ahinko.se>
2015-01-11 21:48:52 +0100

    Added FromUrlEncoded serializer with tests.

---
+++ src/Phapi/Serializer/FormUrlEncoded.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Phapi\Serializer;
+
+use Phapi\Serializer;
+
+/**
+ * JSON Serializer
+ *
+ * Class handling JSON content
+ *
+ * @category Serializer
+ * @package  Phapi
+ * @author   Peter Ahinko <peter@ahinko.se>
+ * @license  MIT (http://opensource.org/licenses/MIT)
+ * @link     https://github.com/ahinko/phapi
+ */
+class FormUrlEncoded extends Serializer
+{
+
+    /**
+     * Mime types that the handler can handle
+     *
+     * @var array
+     */
+    protected $contentTypes = [
+        'application/x-www-form-urlencoded'
+    ];
+
+    /**
+     * Converts input to an php array
+     *
+     * @param $input
+     *
+     * @return array|mixed
+     */
+    public function unserialize($input)
+    {
+        return $input;
+    }
+
+    /**
+     * Converts an array to form url encoded format
+     *
+     * @param $input
+     * @return mixed|string
+     */
+    public function serialize($input)
+    {
+        return $input;
+    }
+}
